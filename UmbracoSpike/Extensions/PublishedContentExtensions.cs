90d1b7f1e8824d8a2c8870586c89f32f28dfeb88
Valter Berg <valter.berg@bouvet.no>
2024-08-26 15:56:19 +0200

    initial commit

---
+++ UmbracoSpike/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace UmbracoSpike.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static HomePage? GetHomePage(this IPublishedContent publishedContent)
+        {
+            return publishedContent.AncestorOrSelf<HomePage>();
+        }
+
+        public static SiteSetting? GetSiteSetting(this IPublishedContent publishedContent)
+        {
+            var homePage = GetHomePage(publishedContent);
+            if (homePage == null)
+            {
+                return null;
+            }
+            return homePage?.FirstChild<SiteSetting>();
+        }
+    }
+}
